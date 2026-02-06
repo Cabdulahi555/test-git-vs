c9ae1b58e0bc77b45efa5ce0b08c41db416b7be2
Cabdulahi Hussein <abdulahih0614@gmail.com>
2026-02-06 15:40:35 -0800

    New commit update

--- test.cpp
+++ test.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main()
 {
     cout <<"Hello CS187!" << endl;
+
+    cout <<"This second message is a updated code from the branch test-update1" << endl;
     return 0;
-}
+} 
